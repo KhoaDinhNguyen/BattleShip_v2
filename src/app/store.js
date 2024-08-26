2748f60669499f56e2eca4c6344de942396c68d4
Nguyễn Khoa <khoacode1305@gmail.com>
2024-08-25 20:33:32 -0700

    feat: add store

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const reducers = {
+
+};
+
+const rootReducer = combineReducers(reducers);
+
+const store = configureStore({reducer: rootReducer});
+export default store;
